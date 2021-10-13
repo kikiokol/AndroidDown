a59a1760e9bc2eeda0e81a9b1955e7c2fb100361
kikiokol <69051007+kikiokol@users.noreply.github.com>
2021-10-13 15:07:04 +0200

    Can now install apps from url's

--- androidDown.py
+++ androidDown.py
@@ -5,6 +5,7 @@
 import subprocess
 import platform
 import os
+import urllib.request
 from pyngrok import ngrok
 from termcolor import colored
 from threading import Thread
@@ -35,30 +36,22 @@ initmsg = """
 ██║  ██║██║ ╚████║██████╔╝██║  ██║╚██████╔╝██║██████╔╝      ██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║
 ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝╚═════╝       ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝   
 
-
+ ● https://github.com/kikiokol/AndroidDown/
 
 """
 
+def detect_url(url: str):
+    if("http" in url):
+        return True
+    else:
+        return False
+
 class commands():
     def help():
-        helpmsg = """
-start - Start an application on the device
-list-apps - List all the installed applications on the device
-rdp-start - Starts scrcpy (https://github.com/Genymobile/scrcpy)
-meterpreter-start - Starts a meterpreter session on the device
-meterpreter-install - Installs a meterpreter payload on the device
-install - Installs a local application on the device
-uninstall - Uninstalls an application on the device
-uninstall-all - Uninstalls every application listed by the list-apps command
-shell - Starts an interactive adb shell session
-get-users - Prints all the users on the device
-restart - Attempts to reboot the device
-destroy - Runs the dangerous command "rm -rf /*" (Requires root)
-help - Prints all the available commands
-exit - Exits the program
-clear - Clears the terminal
-        """
-        print(helpmsg)
+        print("Available commands:\n")
+        for key, value in cmdb.items():
+            print(key)
+        print()
 
     def clear():
         os.system("clear")
@@ -70,8 +63,14 @@ clear - Clears the terminal
     def adb_install():
         apkPath = input("(APK path)... ")
         commands.cli_command("adb shell settings put global install_non_market_apps 1")
+        if(detect_url(apkPath)):
+            #Download apk
+            urllib.request.urlretrieve(apkPath, "apkfile.apk")
+            apkPath = "apkfile.apk"
+        else:
+            os.system("mv {} apkfile.apk".format(apkPath))
         try:
-            output = commands.cli_command("adb install " + apkPath)
+            output = commands.cli_command("adb install apkfile.apk")
             print(output)
         except:
             print("Sorry, an error occurred.")
